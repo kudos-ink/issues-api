9e656e0341cf692eab9212141445bdba87f8f028
Leandro <leapalazzolo@gmail.com>
2023-12-02 13:29:28 -0300

    reactor: api init

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,41 @@
-use warp::Filter;
 mod types;
 
+use warp::Filter;
+
 use crate::types::ApiConfig;
-use sqlx::postgres::PgPoolOptions;
+
+mod db;
+mod handlers;
+mod health;
 
 #[tokio::main]
 async fn main() {
     let ApiConfig {
         http_server_host: host,
         http_server_port: port,
-        database_url
+        database_url,
+        database_init_file,
     } = ApiConfig::new();
 
-    let db_pool = PgPoolOptions::new()
-        .max_connections(1)
-        .connect(&database_url)
+    // init db
+    let db_pool = db::pool::create_pool(&database_url).expect("database pool can be created");
+
+    // TODO: use migrations
+    db::pool::init_db(&db_pool, &database_init_file)
         .await
-        .expect("Failed to create the database pool.");
+        .expect("database can be initialized");
 
-    let routes = warp::any().map(|| warp::reply::html("Hello, world!"));
+    let health_route = health::routes::routes(db_pool);
+    let error_handler = handlers::error_handler;
 
-    let addr = format!("{}:{}", host, port).parse::<std::net::SocketAddr>().expect("Invalid server address");
+    // string all the routes together
+    let routes = health_route
+        .with(warp::cors().allow_any_origin())
+        .recover(error_handler);
+
+    let addr = format!("{}:{}", host, port)
+        .parse::<std::net::SocketAddr>()
+        .expect("Invalid server address");
 
     warp::serve(routes).run(addr).await;
 }
