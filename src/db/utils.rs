ac247d72804a12de1918af600d61571aedcd15c4
Igor Papandinas <igor.papandinas@posteo.net>
2023-12-02 19:46:01 +0100

    generic query executor helper

--- src/db/utils.rs
+++ src/db/utils.rs
@@ -1,10 +1,11 @@
+use mobc_postgres::tokio_postgres::types::ToSql;
 use tokio::time::{timeout, Duration};
 use warp::reject;
 use std::convert::Infallible;
 
 use warp::Filter;
 
-use super::{types::DBPool, errors::DBError};
+use super::{types::DBPool, errors::DBError, pool::get_db_con};
 
 pub const DB_QUERY_TIMEOUT: Duration = Duration::from_secs(5);
 
@@ -12,14 +13,18 @@ pub fn with_db(db_pool: DBPool) -> impl Filter<Extract = (DBPool,), Error = Infa
     warp::any().map(move || db_pool.clone())
 }
 
-pub async fn execute_operation_with_timeout<F, T, E>(future: F, duration: Duration) -> Result<T, reject::Rejection>
-where
-    F: std::future::Future<Output = Result<T, E>>,
-    E: Into<DBError>,
-{
-    match timeout(duration, future).await {
-        Ok(Ok(result)) => Ok(result),
-        Ok(Err(err)) => Err(reject::custom(err.into())),
-        Err(_elapsed) => Err(reject::custom(DBError::DBTimeout(_elapsed))),
-    }
+pub async fn execute_query_with_timeout(
+    db_pool: &DBPool,
+    query: &str,
+    params: &[&(dyn ToSql + Sync)],
+    timeout_duration: Duration,
+) -> Result<(), reject::Rejection> {
+    let db_conn = get_db_con(db_pool).await.map_err(reject::custom)?;
+
+    timeout(timeout_duration, db_conn.execute(query, params))
+        .await
+        .map_err(|err| reject::custom(DBError::DBTimeout(err)))?
+        .map_err(|err| reject::custom(DBError::DBQuery(err)))?;
+
+    Ok(())
 }
--- src/health/handlers.rs
+++ src/health/handlers.rs
@@ -1,10 +1,7 @@
-use crate::db::{types::DBPool, self, utils::{execute_operation_with_timeout, DB_QUERY_TIMEOUT}};
-use warp::{http::StatusCode, reject,  Reply, Rejection};
+use crate::db::{types::DBPool, utils::{execute_query_with_timeout, DB_QUERY_TIMEOUT}};
+use warp::{http::StatusCode,  Reply, Rejection};
 
 pub async fn health_handler(db_pool: DBPool) -> Result<impl Reply, Rejection> {
-    let db_conn = db::pool::get_db_con(&db_pool).await.map_err(reject::custom)?;
-
-    execute_operation_with_timeout(db_conn.execute("SELECT 1", &[]), DB_QUERY_TIMEOUT).await?;
-
+    execute_query_with_timeout(&db_pool, "SELECT 1", &[], DB_QUERY_TIMEOUT).await?;
     Ok(StatusCode::OK)
 }
