545eefa02c98958426d7b6ec614bc89b27da682c
Igor Papandinas <igor.papandinas@posteo.net>
2023-12-02 19:38:04 +0100

    generic timeout helper

--- src/db/utils.rs
+++ src/db/utils.rs
@@ -1,9 +1,25 @@
+use tokio::time::{timeout, Duration};
+use warp::reject;
 use std::convert::Infallible;
 
 use warp::Filter;
 
-use super::types::DBPool;
+use super::{types::DBPool, errors::DBError};
+
+pub const DB_QUERY_TIMEOUT: Duration = Duration::from_secs(5);
 
 pub fn with_db(db_pool: DBPool) -> impl Filter<Extract = (DBPool,), Error = Infallible> + Clone {
     warp::any().map(move || db_pool.clone())
+}
+
+pub async fn execute_operation_with_timeout<F, T, E>(future: F, duration: Duration) -> Result<T, reject::Rejection>
+where
+    F: std::future::Future<Output = Result<T, E>>,
+    E: Into<DBError>,
+{
+    match timeout(duration, future).await {
+        Ok(Ok(result)) => Ok(result),
+        Ok(Err(err)) => Err(reject::custom(err.into())),
+        Err(_elapsed) => Err(reject::custom(DBError::DBTimeout(_elapsed))),
+    }
 }
--- src/health/handlers.rs
+++ src/health/handlers.rs
@@ -1,16 +1,10 @@
-use crate::db::{types::DBPool, errors::DBError, self};
+use crate::db::{types::DBPool, self, utils::{execute_operation_with_timeout, DB_QUERY_TIMEOUT}};
 use warp::{http::StatusCode, reject,  Reply, Rejection};
-use tokio::time::{timeout, Duration};
-
-const QUERY_TIMEOUT: Duration = Duration::from_secs(5);
 
 pub async fn health_handler(db_pool: DBPool) -> Result<impl Reply, Rejection> {
     let db_conn = db::pool::get_db_con(&db_pool).await.map_err(reject::custom)?;
 
-    timeout(QUERY_TIMEOUT, db_conn.execute("SELECT 1", &[]))
-        .await
-        .map_err(|err| reject::custom(DBError::DBTimeout(err)))? // Handle the timeout error
-        .map_err(|err| reject::custom(DBError::DBQuery(err)))?; // Handle the query error
+    execute_operation_with_timeout(db_conn.execute("SELECT 1", &[]), DB_QUERY_TIMEOUT).await?;
 
     Ok(StatusCode::OK)
 }
