debc90d9b52e9a840f5e46de7339905d7f099e29
Leandro <leapalazzolo@gmail.com>
2023-12-02 13:28:48 -0300

    feat: add error handler

---
+++ src/handlers.rs
@@ -0,0 +1,52 @@
+
+use std::convert::Infallible;
+use serde::Serialize;
+use warp::{hyper::StatusCode, Rejection, Reply};
+
+use crate::db::errors::DBError;
+
+#[derive(Serialize)]
+struct ErrorResponse {
+    message: String,
+}
+
+pub async fn error_handler(err: Rejection) -> std::result::Result<impl Reply, Infallible> {
+    let code;
+    let message;
+
+    if err.is_not_found() {
+        code = StatusCode::NOT_FOUND;
+        message = "Not Found";
+    } else if err
+        .find::<warp::filters::body::BodyDeserializeError>()
+        .is_some()
+    {
+        code = StatusCode::BAD_REQUEST;
+        message = "Invalid Body";
+    } else if let Some(e) = err.find::<DBError>() {
+        match e {
+            DBError::DBQuery(_) => {
+                code = StatusCode::BAD_REQUEST;
+                message = "Could not Execute request";
+            }
+            _ => {
+                eprintln!("unhandled application error: {:?}", err);
+                code = StatusCode::INTERNAL_SERVER_ERROR;
+                message = "Internal Server Error";
+            }
+        }
+    } else if err.find::<warp::reject::MethodNotAllowed>().is_some() {
+        code = StatusCode::METHOD_NOT_ALLOWED;
+        message = "Method Not Allowed";
+    } else {
+        eprintln!("unhandled error: {:?}", err);
+        code = StatusCode::INTERNAL_SERVER_ERROR;
+        message = "Internal Server Error";
+    }
+
+    let json = warp::reply::json(&ErrorResponse {
+        message: message.into(),
+    });
+
+    Ok(warp::reply::with_status(json, code))
+}
