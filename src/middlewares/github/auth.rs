d8b37d88aeb422e62635a879458dd05141f0f861
Leandro Palazzolo <leapalazzolo@gmail.com>
2025-01-11 17:08:27 -0300

    chore: test

--- src/middlewares/github/auth.rs
+++ src/middlewares/github/auth.rs
@@ -2,13 +2,14 @@ use crate::middlewares::{
     errors::AuthenticationError,
     utils::{token_from_header, BEARER},
 };
-use reqwest::Client;
+use surf;
+use surf::http::headers::{AUTHORIZATION, USER_AGENT};
 
 use warp::{
     http::header::{HeaderMap, HeaderValue},
     reject, Filter, Rejection,
 };
-use log::error;
+use log::{error, info};
 
 use super::model::GitHubUser;
 
@@ -21,61 +22,58 @@ pub fn with_github_auth() -> impl Filter<Extract = (GitHubUser,), Error = Reject
 async fn authorize(headers: HeaderMap<HeaderValue>) -> Result<GitHubUser, Rejection> {
     match token_from_header(&headers, BEARER) {
         Ok(token) => {
-            let client = Client::new();
-            let response = client
-                .get("https://api.github.com/user") // 5,000 requests per hour for auth'd requests
-                .header("Authorization", format!("{BEARER} {token}"))
-                .header("User-Agent", "MoreKudos")
-                .send()
+            info!("{BEARER} {token}"); // TODO: test
+            let mut response = surf::get("https://api.github.com/user")
+                .header(AUTHORIZATION, format!("{BEARER} {token}"))
+                .header(USER_AGENT, "MoreKudos")
                 .await
-                .map_err(|e| {                    
-                    error!(
-                    "Error calling GitHub API: {e}",
-                );
+                .map_err(|e| {
+                    error!("Error calling GitHub API: {e}");
                     AuthenticationError::GitHub
                 })?;
-
-                if response.status().is_success() {
-                    let user_data: serde_json::Value = response
-                        .json()
-                        .await
-                        .map_err(|_| reject::custom(AuthenticationError::GitHub))?;
     
-                    // Extract user ID and username
-                    let id = user_data
-                        .get("id")
-                        .and_then(|v| v.as_i64())
-                        .ok_or_else(|| reject::custom(AuthenticationError::GitHub))?;
-                    let username = user_data
-                        .get("login")
-                        .and_then(|v| v.as_str())
-                        .ok_or_else(|| reject::custom(AuthenticationError::GitHub))?
-                        .to_string();
+            if response.status().is_success() {
+                let user_data: serde_json::Value = response
+                    .body_json()
+                    .await
+                    .map_err(|_| reject::custom(AuthenticationError::GitHub))?;
     
-                    let avatar_url = user_data// user for new users
-                        .get("avatar_url")
-                        .and_then(|v| v.as_str())
-                        .ok_or_else(|| reject::custom(AuthenticationError::GitHub))?
-                        .to_string();
-                    Ok(GitHubUser { id, username, avatar_url })
+                // Extract user ID and username
+                let id = user_data
+                    .get("id")
+                    .and_then(|v| v.as_i64())
+                    .ok_or_else(|| reject::custom(AuthenticationError::GitHub))?;
+                let username = user_data
+                    .get("login")
+                    .and_then(|v| v.as_str())
+                    .ok_or_else(|| reject::custom(AuthenticationError::GitHub))?
+                    .to_string();
+    
+                let avatar_url = user_data
+                    .get("avatar_url")
+                    .and_then(|v| v.as_str())
+                    .ok_or_else(|| reject::custom(AuthenticationError::GitHub))?
+                    .to_string();
+    
+                Ok(GitHubUser { id, username, avatar_url })
+            } else {
+                let status = response.status();
+                let body = response
+                    .body_string()
+                    .await
+                    .unwrap_or_else(|_| "Unable to read body".to_string());
+                error!(
+                    "GitHub token validation failed. Status: {:?}, Body: {}",
+                    status, body
+                );
+    
+                if status.is_client_error() {
+                    Err(reject::custom(AuthenticationError::WrongCredentials))
                 } else {
-                    let status = response.status();
-                    let body = response
-                        .text()
-                        .await
-                        .unwrap_or_else(|_| "Unable to read body".to_string());
-                    error!(
-                        "GitHub token validation failed. Status: {:?}, Body: {}",
-                        status, body
-                    );
-    
-                    if status.is_client_error() {
-                        Err(reject::custom(AuthenticationError::WrongCredentials))
-                    } else {
-                        Err(reject::custom(AuthenticationError::GitHub))
-                    }
+                    Err(reject::custom(AuthenticationError::GitHub))
                 }
+            }
         }
         Err(e) => Err(reject::custom(e)),
     }
-}
+}    
